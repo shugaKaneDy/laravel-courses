ceacbb896a68e59b9346148c41793527f0489e76
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-06-05 21:05:10 +0800

    Pagination

--- ideas/app/Http/Controllers/DashboardController.php
+++ ideas/app/Http/Controllers/DashboardController.php
@@ -18,7 +18,8 @@ class DashboardController extends Controller
         /* dump(Idea::all()); */
         return view('dashboard', [
             /* 'ideas' => Idea::all() */
-            'ideas' => Idea::orderBy('created_at', 'DESC')->get()
+            /* 'ideas' => Idea::orderBy('created_at', 'DESC')->get() */
+            'ideas' => Idea::orderBy('created_at', 'DESC')->paginate(5)
         ]);
     }
 
